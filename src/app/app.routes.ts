db5be6e4869e7b4597038a580abdd6d4294d6611
JhonnatanDouglas <jhonnatanaraujoDEV@gmail.com>
2024-04-04 14:44:35 -0300

    revisao 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { AboutComponent } from './components/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'contatos', component: ContactsComponent },
+  { path: 'sobre', component: AboutComponent },
+];
