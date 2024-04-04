06a7d4e64adab80628edcab0b35ae6835b62b7da
JhonnatanDouglas <jhonnatanaraujoDEV@gmail.com>
2024-04-04 15:08:40 -0300

    pratica e relembrando

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'contatos', component: ContactComponent },
+  { path: 'sobre', component: SobreComponent },
+];
